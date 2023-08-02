cde4150662b4b7827c440c962c4239e24aabbda8
Troy Krause <troykrause07@gmail.com>
2023-08-02 20:32:57 +0200

    MovieRating

---
+++ MovieRatingActivity/src/main/java/com/sem2/movieratingactivity/MovieRatingActivity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sem2.movieratingactivity;
+
+/**
+ *
+ * @author troyk
+ */
+public class MovieRatingActivity {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
